2034ce13e7b63d7c7f9e5fdcab924420d2107800
Ciaran Cruz <C21318233@mytudublin.ie>
2023-02-13 22:21:29 +0000

    Mode 1 (Circles) and MOdde 2 (Rectangles and squares) completed

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -5,31 +5,40 @@ import processing.core.PApplet;
 public class Loops extends PApplet {
 
 	int mode = 0;
+	int type = 0;
 
 	public void settings() {
-		size(1000, 1000);
-		//fullScreen(SPAN);
+		size(500, 500);
+		// fullScreen(SPAN);
 	}
 
 	public void setup() {
-		colorMode(HSB);
+		colorMode(RGB, 255, 255, 255);
 	}
 
 	public void keyPressed() {
-
 		mode = key - '0';
-		println(mode);
+	}
+
+	public void mousePressed() {
+		if (type == 1)
+		{
+			type = 0;
+		}
+		else
+		{
+			type++;
+		}
 	}
 
 	float off = 0;
 
 	public void draw() {
 		background(0);
-		fill(255);
+		fill(0, 255, 0);
 		noStroke();
-
 		switch (mode) {
-			case 0:
+			case 0: // Circles
 				int numCircles = (int) max(1, mouseX / 50.0f);
 				float d = width / numCircles;
 				for (int j = 0; j < numCircles; j++) {
@@ -43,8 +52,52 @@ public class Loops extends PApplet {
 				}
 				off += (mouseY / 50.0f);
 				break;
-			case 1:
+
+			case 1: // Rectangles and squares
+				switch (type)
+				{
+					case 0:
+						fill(0, 255, 0);
+						if(mouseX < 250)
+						{
+							rect(0, 0, 250, 500);
+						}
+						else 
+						{
+							rect(250, 0, 250, 500);
+						}
+						break;
+
+
+					case 1:
+						fill(0, 255, 0);
+						if(mouseX < 250 && mouseY < 250)
+						{
+							rect(0, 0, 250, 250);
+						}
+						else if(mouseX > 250 && mouseY < 250)
+						{
+							rect(250, 0, 250, 250);
+						}
+						else if(mouseX < 250 && mouseY > 250)
+						{
+							rect(0, 250, 250, 250);
+						}
+						else 
+						{
+							rect(250, 250, 250, 250);
+						}
+						break;
+
+					default:
+						break;
+				}
 				break;
+
+			case 2:
+
+				break;
+
 			default:
 				break;
 		}
