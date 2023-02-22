318fec13516b0f19df236ebadcceb911be608901
Ciaran Cruz <C21318233@mytudublin.ie>
2023-02-22 10:24:01 +0000

    Copied code from Dog.java into Cat.java

--- java/src/ie/tudublin/Cat.java
+++ java/src/ie/tudublin/Cat.java
@@ -1,5 +1,24 @@
 package ie.tudublin;
 
-public class Cat {
-    
+public class Cat
+{
+	String name;
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public Cat()
+	{
+	}
+	
+	public Cat(String name)
+	{
+	}
+	
+	public void speak()
+	{
+		System.out.println("Woof");
+	}
 }
