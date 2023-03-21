9ad6e8d15dbe8a0979527b0ab34addb7b1843201
Ciaran Cruz <C21318233@mytudublin.ie>
2023-03-21 16:18:16 +0000

    Implemented spell function

--- java/src/ie/tudublin/PitchSpeller.java
+++ java/src/ie/tudublin/PitchSpeller.java
@@ -4,9 +4,23 @@ public class PitchSpeller {
     float[] frequencies = {293.66f, 329.63f, 369.99f, 392.00f, 440.00f, 493.88f, 554.37f, 587.33f, 659.25f, 739.99f, 783.99f, 880.00f, 987.77f, 1108.73f, 1174.66f};
     String[] spellings = {"D,", "E,", "F,", "G,", "A,", "B,", "C", "D", "E", "F", "G", "A", "B","c", "d", "e", "f", "g", "a", "b", "c'", "d'", "e'", "f'", "g'", "a'", "b'", "c''", "d''"};
     
+    
     public String spell(float frequency)
     {
-        return 
+        int index = 0;
+        float distance = Math.abs(frequency - frequencies[0]);
+
+        for (int i = 1; i < frequencies.length; i++)
+        {
+            float newDistance = Math.abs(frequency - frequencies[i]);
+            if (newDistance < distance)
+            {
+               distance = newDistance;
+               index = i; 
+            }
+        }
+        
+        return spellings[index];
     }
 
     
